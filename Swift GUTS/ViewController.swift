85b58ec704f0224bba595d81857701b203ca30b1
Hank <shinygun@gmail.com>
2015-02-20 21:36:13 -0600

    Initial Commit

---
+++ Swift GUTS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift GUTS
+//
+//  Created by Kiedrowski on 2/20/15.
+//  Copyright (c) 2015 Clockwork. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
