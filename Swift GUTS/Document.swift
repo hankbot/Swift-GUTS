0c811ee22d92c0beec98df421f1d20c2e86808d5
Hank <shinygun@gmail.com>
2015-02-20 22:48:13 -0600

    Resolved tabs for now

--- Swift GUTS/Document.swift
+++ Swift GUTS/Document.swift
@@ -9,26 +9,26 @@
 import Cocoa
 
 class Document: NSPersistentDocument {
-
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
-
-    override func windowControllerDidLoadNib(aController: NSWindowController) {
-        super.windowControllerDidLoadNib(aController)
-        // Add any code here that needs to be executed once the windowController has loaded the document's window.
-    }
-
-    override class func autosavesInPlace() -> Bool {
-        return true
-    }
-
-    override func makeWindowControllers() {
-        // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: "Main", bundle: nil)!
-        let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
-        self.addWindowController(windowController)
-    }
-
+	
+	override init() {
+		super.init()
+		// Add your subclass-specific initialization here.
+	}
+	
+	override func windowControllerDidLoadNib(aController: NSWindowController) {
+		super.windowControllerDidLoadNib(aController)
+		// Add any code here that needs to be executed once the windowController has loaded the document's window.
+	}
+	
+	override class func autosavesInPlace() -> Bool {
+		return true
+	}
+	
+	override func makeWindowControllers() {
+		// Returns the Storyboard that contains your Document window.
+		let storyboard = NSStoryboard(name: "Main", bundle: nil)!
+		let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
+		self.addWindowController(windowController)
+	}
+	
 }
